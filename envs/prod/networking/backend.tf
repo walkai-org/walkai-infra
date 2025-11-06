75ed7f4f847fe5d8162c100dff20259cbfaddb38
gtangari10 <gtangari@correo.um.edu.uy>
2025-11-06 16:26:42 -0300

    modifying backend

--- envs/prod/networking/backend.tf
+++ envs/prod/networking/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "walkai-terraform-state"
     key            = "prod/networking/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "walkai-terraform-locks"
     encrypt        = true
   }
 }
