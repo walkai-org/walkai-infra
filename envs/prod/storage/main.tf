bc200094e6ec6a71c3fbfb348479f6979137a300
gtangari10 <gtangari@correo.um.edu.uy>
2025-11-09 22:25:14 -0300

    changing bucket name to avoid error

--- envs/prod/storage/main.tf
+++ envs/prod/storage/main.tf
@@ -6,7 +6,7 @@ module "storage" {
   source = "../../../modules/storage"
 
   table_name  = "walkai_cluster_cache"
-  bucket_name = "walkaiorg.app-client"
+  bucket_name = "walkaiorg.app-client2"
   tags = {
     Environment = "prod"
     Project     = "walkai"
