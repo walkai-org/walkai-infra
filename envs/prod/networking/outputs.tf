26a3bbbedffae43abe50bb145a66fcd4cf9d938f
gtangari10 <gtangari@correo.um.edu.uy>
2025-11-19 19:12:20 -0300

    feat: networking outputs

---
+++ envs/prod/networking/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "Identifier of the VPC created by the networking module."
+  value       = module.networking.vpc_id
+}
+
+output "subnet_ids" {
+  description = "Subnets created by the networking module keyed by logical name."
+  value       = module.networking.subnet_ids
+}
+
+output "default_security_group_id" {
+  description = "Default security group ID associated with the VPC."
+  value       = module.networking.default_security_group_id
+}
