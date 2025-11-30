77faf1eccd746a7f51678dce72e8e7a57b2cbf81
gtangari10 <gtangari@correo.um.edu.uy>
2025-11-30 10:58:55 -0300

    fix: output

--- envs/prod/storage/outputs.tf
+++ envs/prod/storage/outputs.tf
@@ -7,3 +7,7 @@ output "oauth_table_arn" {
   description = "OAuth DynamoDB table ARN."
   value       = module.storage.oauth_table_arn
 }
+output "info_bucket_arn" {
+  description = "Info bucket ARN."
+  value       = module.storage.info_bucket_arn
+}
