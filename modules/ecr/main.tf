468dbdde35f663bf0f1e62b19a299d04f8b4b8a1
gtangari10 <gtangari@correo.um.edu.uy>
2026-01-24 19:12:32 -0300

    fix: forcing delete on ecr images

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,11 +1,13 @@
 resource "aws_ecr_repository" "api" {
   name                 = var.repository_name
   image_tag_mutability = var.image_tag_mutability
+  force_delete         = true
   tags                 = var.tags
 }
 
 resource "aws_ecr_repository" "users" {
   name                 = var.users_repository_name
   image_tag_mutability = var.image_tag_mutability
+  force_delete         = true
   tags                 = var.tags
 }
