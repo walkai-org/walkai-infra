d3750f4c0af1b44adab3e4cce8285c39908084c3
gtangari10 <gtangari@correo.um.edu.uy>
2026-01-27 18:18:06 -0300

    feat: parametrization of the vpc_cidr

--- envs/prod/networking/main.tf
+++ envs/prod/networking/main.tf
@@ -6,33 +6,33 @@ module "networking" {
   source = "../../../modules/networking"
 
   name     = "walkai-prod-networking"
-  vpc_cidr = "172.31.0.0/16"
+  vpc_cidr = var.vpc_cidr
   region   = var.region
 
   subnets = {
     public_a = {
       name                   = "walkai-public-subnet-a-az"
-      cidr_block             = "172.31.96.0/20"
+      cidr_block             = cidrsubnet(var.vpc_cidr, 2, 1)
       availability_zone      = "us-east-1a"
       public                 = true
       map_public_ip_on_launch = true
     }
     private_a = {
       name                 = "walkai-private-subnet-a-az"
-      cidr_block           = "172.31.0.0/20"
+      cidr_block           = cidrsubnet(var.vpc_cidr, 2, 0)
       availability_zone    = "us-east-1a"
       public               = false
     }
     public_b = {
       name                   = "walkai-public-subnet-b-az"
-      cidr_block             = "172.31.112.0/20"
+      cidr_block             = cidrsubnet(var.vpc_cidr, 2, 3)
       availability_zone      = "us-east-1b"
       public                 = true
       map_public_ip_on_launch = true
     }
     private_b = {
       name                 = "walkai-private-subnet-b-az"
-      cidr_block           = "172.31.80.0/20"
+      cidr_block           = cidrsubnet(var.vpc_cidr, 2, 2)
       availability_zone    = "us-east-1b"
       public               = false
     }
--- envs/prod/networking/variables.tf
+++ envs/prod/networking/variables.tf
@@ -1,3 +1,9 @@
 variable "region" {
   default = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "172.31.0.0/16"
+}
